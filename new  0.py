37ba13d4167dbeb0b15438beffd82a770a3bb6ac
williamchi1996 <william60608@yahoo.com.tw>
2014-10-13 14:37:31 +0800

    10/13

---
+++ new  0.py
@@ -0,0 +1,12 @@
+x = input("which is your country ? \n Taipei,Keelung,Kaohsiung,Taoyuan? \n ")
+
+if x == Taipei:
+    
+    print("101")
+elif x == Keelung:
+    print("NIGHTMARKET")
+elif x == Kaohsiung:
+    print("85 BIG BUILDING")
+else:
+    print("AIRPORT")
+
